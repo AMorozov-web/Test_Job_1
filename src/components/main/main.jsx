58e89e4578807c201155dc26ef6f7af9a7a8881d
Alex Morozov <a.morozov.work@mail.ru>
2021-06-20 23:47:00 +0300

    feat: add promo component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Promo} from '../promo/promo';
 
 import './main.scss';
 
 const Main = () => {
   return (
     <main className="main page-body__main">
-      Main
+      <Promo />
     </main>
   );
 };
--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './promo.scss';
+
 const Promo = () => {
   return (
     <section className="promo main__promo">
-      
+      <div className="promo__container">
+        promo
+      </div>
     </section>
   );
 };
