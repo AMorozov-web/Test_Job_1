ee55b24a38a1c640f61f447c362d4e2903d10a5c
Alex Morozov <a.morozov.work@mail.ru>
2021-06-21 23:04:18 +0300

    fix: convert logo to svg

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -5,7 +5,7 @@ import './logo.scss';
 const Logo = () => {
   return (
     <div className="logo header__logo">
-      <img className="logo__image" src="./img/logo.png" alt="Logo" />
+      <img className="logo__image" src="./img/logo.svg" alt="Logo" />
     </div>
   );
 };
