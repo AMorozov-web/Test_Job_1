5ef411ed4ed5bfac77e04eaa7f048596e4091c1b
Alex Morozov <a.morozov.work@mail.ru>
2021-06-23 22:12:29 +0300

    feat: complete promo component

--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -8,7 +8,11 @@ const Promo = () => {
       <div className="promo__container">
         <div className="promo__content">
           react-modal
-          <button className="promo__play-btn" type="button" aria-label="Play promo video"></button>
+          <button className="promo__play-btn" type="button" aria-label="Play promo video">
+            <svg className="promo__play-btn-icon" width="34" height="34" viewBox="0 0 34 34" xmlns="http://www.w3.org/2000/svg">
+              <path d="M24.0833 17L12.75 8.5V25.5L24.0833 17Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </button>
         </div>
       </div>
     </section>
