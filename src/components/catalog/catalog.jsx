c5ef8dd45994d678457cc2ec80bc7d2e9133a799
Alex Morozov <a.morozov.work@mail.ru>
2021-06-26 11:10:12 +0300

    feat: complete catalog component

--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import {SORT_OPTIONS} from '../../const';
 
 import './catalog.scss';
+
+const getOption = (option) => {
+  return (
+    <option key={option.value} value={option.value}>{option.text}</option>
+  );
+};
 
 const Catalog = () => {
 
   return (
     <section className="catalog main__catalog">
-      <h1 className="catalog__title">
+      <h2 className="catalog__title">
         Фотобудки
-      </h1>
+      </h2>
       <div className="catalog__sort">
-        <p className="catalog__sort-title">Сортировка:</p>
+        <label className="catalog__sort-title" htmlFor="sort-select">Сортировка:</label>
+        <select className="catalog__sort-select" id="sort-select" defaultValue="default">
+          {SORT_OPTIONS.map(getOption)}
+        </select>
       </div>
+      <ul className="catalog__list">
+        list
+      </ul>
     </section>
   );
 };
---
+++ src/const.js
@@ -0,0 +1,18 @@
+const SORT_OPTIONS = [
+  {
+    value: `default`,
+    text: `По умолчанию`,
+  },
+  {
+    value: `ascending`,
+    text: `По возрастанию`,
+  },
+  {
+    value: `descending`,
+    text: `По убыванию`,
+  },
+];
+
+export {
+  SORT_OPTIONS,
+};
