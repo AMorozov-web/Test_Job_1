be08629913a0b24e8e1fd9e46eaca71bed1569c0
Alex Morozov <a.morozov.work@mail.ru>
2021-06-28 20:50:17 +0300

    feat: add img to promo

--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -10,7 +10,7 @@ const Promo = () => {
       </h2>
       <div className="promo__container">
         <div className="promo__content">
-          react-modal
+          <img src="img/promo.png" alt="Promo video" width="315" height="188" className="promo__img" />
           <button className="promo__play-btn" type="button" aria-label="Play promo video">
             <svg className="promo__play-btn-icon" width="34" height="34" viewBox="0 0 34 34" xmlns="http://www.w3.org/2000/svg">
               <path d="M24.0833 17L12.75 8.5V25.5L24.0833 17Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
